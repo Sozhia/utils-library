abf7c1345b52f51b97866bab7e4fc05f5b56ca11
Miqueas <coding4canary@gmail.com>
2024-02-22 14:09:14 +0000

    VSCODE, JAVA, ECLIPSE

--- utils-copiado/src/es/ull/iis/function/TimeFunctionParams.java
+++ utils-copiado/src/es/ull/iis/function/TimeFunctionParams.java
@@ -1,13 +1,16 @@
-/**
- * 
- */
+
 package es.ull.iis.function;
 
-/**
- * A generic interface to wrap the parameters that can receive a {@link #TimeFunction} object. 
- * @author Iván Castilla
- *
- */
+// This code snippet is defining a Java interface named `TimeFunctionParams`. This
+// interface declares a single method `getTime()` without any implementation.
+// Classes that implement this interface will need to provide their own
+// implementation for the `getTime()` method. This interface serves as a contract
+// specifying that any class implementing it must have a method to retrieve a
+// double value representing time.
 public interface TimeFunctionParams {
+	// The code snippet is defining a Java interface named `TimeFunctionParams`. This
+	// interface declares a single method `getTime()` without any implementation.
+	// Classes that implement this interface will need to provide their own
+	// implementation for the `getTime()` method.
 	public double getTime();
 }
