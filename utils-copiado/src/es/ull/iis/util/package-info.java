832d49f64039398579d4a3977c312b9348aa0f65
Miqueas <coding4canary@gmail.com>
2024-02-22 12:31:25 +0000

    Punto de partida

---
+++ utils-copiado/src/es/ull/iis/util/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides miscellaneous utility classes, such as simulation cycle management 
+ * ({@link es.ull.iis.util.cycle.Cycle}, {@link es.ull.iis.util.cycle.CycleIterator}...), 
+ * complex data structures ({@link es.ull.iis.util.PrioritizedMap}), and other utilities 
+ * ({@link es.ull.iis.util.ClassPathHacker}, {@link es.ull.iis.util.RandomPermutation}).
+ */
+package es.ull.iis.util;
