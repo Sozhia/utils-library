23fbc38e57d0d1410489abb001cf24edc6668124
Miqueas <coding4canary@gmail.com>
2024-02-22 15:11:05 +0000

    Created IFunctionParams

---
+++ utils/src/main/java/ull/tfg/utils/ITimeFunctionParams.java
@@ -0,0 +1,15 @@
+
+package ull.tfg.utils;
+
+/**
+ * The interface Time function params.
+ */
+public interface ITimeFunctionParams {
+
+    /**
+     * Gets time.
+     *
+     * @return the time
+     */
+    double getTime();
+}
