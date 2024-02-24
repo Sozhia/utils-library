e5b432b28c549289172ef2760a1e4728025f0749
Miqueas <coding4canary@gmail.com>
2024-02-24 13:08:01 +0000

    Creacion de clase

---
+++ src/test/java/ull/tfg/utils/PeriodicProportionFunctionTest.java
@@ -0,0 +1,45 @@
+package ull.tfg.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PeriodicProportionFunctionTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void getValue() {
+  }
+
+  @Test
+  void setParameters() {
+  }
+
+  @Test
+  void getNElem() {
+  }
+
+  @Test
+  void setNElem() {
+  }
+
+  @Test
+  void getProp() {
+  }
+
+  @Test
+  void setProp() {
+  }
+
+  @Test
+  void getTimeUnit() {
+  }
+
+  @Test
+  void setTimeUnit() {
+  }
+}
