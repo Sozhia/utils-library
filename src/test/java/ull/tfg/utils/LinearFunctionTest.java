e9dbe881312a21f53973f664264c667696f25ef1
Miqueas <coding4canary@gmail.com>
2024-02-24 10:50:20 +0000

    Agregado test getValue

--- src/test/java/ull/tfg/utils/LinearFunctionTest.java
+++ src/test/java/ull/tfg/utils/LinearFunctionTest.java
@@ -5,49 +5,57 @@ import org.junit.jupiter.api.Test;
 
 class LinearFunctionTest {
 
-    LinearFunction instance = new LinearFunction();
+    LinearFunction constantFunction = new LinearFunction();
     AbstractTimeFunction scale = new ConstantFunction(0.23);
     AbstractTimeFunction shift = new ConstantFunction(0.11);
+
+    TimeFunctionParams params = new TimeFunctionParams() {
+        @Override
+        public double getTime() {
+            return 2.0;
+        }
+    };
 
 
     @Test
     void testSetParameters() {
 
-        instance.setParameters(scale,shift);
-        Assertions.assertEquals(scale, instance.getScale());
-        Assertions.assertEquals(shift, instance.getShift());
+        constantFunction.setParameters(scale,shift);
+        Assertions.assertEquals(scale, constantFunction.getScale());
+        Assertions.assertEquals(shift, constantFunction.getShift());
     }
 
     @Test
     void getScale() {
-        instance.setParameters(scale,shift);
-        Assertions.assertEquals(scale, instance.getScale());
+        constantFunction.setParameters(scale,shift);
+        Assertions.assertEquals(scale, constantFunction.getScale());
     }
 
     @Test
     void getShift() {
-        instance.setParameters(scale,shift);
-        Assertions.assertEquals(shift, instance.getShift());
+        constantFunction.setParameters(scale,shift);
+        Assertions.assertEquals(shift, constantFunction.getShift());
     }
 
     @Test
     void setScale() {
         AbstractTimeFunction newScale = new ConstantFunction(0.15);
-        instance.setScale(newScale);
-        Assertions.assertEquals(newScale, instance.getScale());
-        instance.setScale(scale);
+        constantFunction.setScale(newScale);
+        Assertions.assertEquals(newScale, constantFunction.getScale());
+        constantFunction.setScale(scale);
     }
 
     @Test
     void setShift() {
         AbstractTimeFunction newShift = new ConstantFunction(0.18);
-        instance.setShift(newShift);
-        Assertions.assertEquals(newShift, instance.getShift());
-        instance.setScale(shift);
+        constantFunction.setShift(newShift);
+        Assertions.assertEquals(newShift, constantFunction.getShift());
+        constantFunction.setScale(shift);
     }
 
     @Test
     void getValue() {
+        Assertions.assertEquals(0.23,constantFunction.getValue(params));
 
     }
 
