e103f1ff03753865837865e33f6fe8ce7d40c49c
Miqueas <coding4canary@gmail.com>
2024-03-06 19:11:37 +0000

    Asistida por ChatGPT y corregida por MiqueasGG

--- src/test/java/ull/tfg/utils/RandomFunctionTest.java
+++ src/test/java/ull/tfg/utils/RandomFunctionTest.java
@@ -4,26 +4,47 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import simkit.random.RandomVariate;
+import simkit.random.RandomVariateFactory;
 
 class RandomFunctionTest {
+  RandomFunction randomFunction;
+  TimeFunctionParams params;
 
   @BeforeEach
   void setUp() {
-  }
-
-  @Test
-  void getRandom() {
-  }
-
-  @Test
-  void setRandom() {
+    RandomVariate rnd = RandomVariateFactory.getInstance(
+        "ConstantVariate", 5.0);
+    randomFunction = new RandomFunction(rnd);
+    params = () -> 2.0;
   }
 
   @Test
   void getValue() {
+    assertEquals(5.0, randomFunction.getValue(params));
+  }
+
+  @Test
+  void getRandom() {
+    RandomVariate expectedRnd = randomFunction.getRandom();
+    assertNotNull(expectedRnd);
+  }
+
+  @Test
+  void setRandom() {
+    RandomVariate newRnd = RandomVariateFactory.getInstance(
+        "ConstantVariate", 10.0);
+    randomFunction.setRandom(newRnd);
+    RandomVariate actualRnd = randomFunction.getRandom();
+    assertEquals(newRnd, actualRnd);
   }
 
   @Test
   void setParameters() {
+    RandomVariate newRnd = RandomVariateFactory.getInstance(
+        "ConstantVariate", 10.0);
+    randomFunction.setParameters(newRnd);
+    RandomVariate actualRnd = randomFunction.getRandom();
+    assertEquals(newRnd, actualRnd);
   }
 }
