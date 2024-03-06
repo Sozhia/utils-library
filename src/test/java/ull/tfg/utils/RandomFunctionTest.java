39b48631222f4d793965db33ef536ce4e147c4ee
Miqueas <coding4canary@gmail.com>
2024-03-06 19:09:31 +0000

    Creada estructura

---
+++ src/test/java/ull/tfg/utils/RandomFunctionTest.java
@@ -0,0 +1,29 @@
+package ull.tfg.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RandomFunctionTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void getRandom() {
+  }
+
+  @Test
+  void setRandom() {
+  }
+
+  @Test
+  void getValue() {
+  }
+
+  @Test
+  void setParameters() {
+  }
+}
